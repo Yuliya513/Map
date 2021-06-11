69b53fff3c38774836c92cbc145b9809024279f5
Yuliya513 <yuliya.senik@gmail.com>
2021-06-11 14:28:36 +0300

    Initial commit

---
+++ src/com/company/Mapa.java
@@ -0,0 +1,36 @@
+package com.company;
+import java.util HashMap;
+import java.util.Map
+
+public class Mapa {
+
+    public static void main(String[] args) {
+	String FirstName A;
+	String LastName B;
+        String FirstName D;
+        String LastName B1;
+        String FirstName Aol;
+        String LastName Bac
+        String FirstName Andi;
+        String LastName Bee;
+        String FirstName Aa1;
+        String LastName Bs;
+        String FirstName A;
+        String LastName Bh;
+        String FirstName Ao;
+        String LastName Bo;
+        String FirstName Ay;
+        String LastName By;
+        String FirstName Al;
+        String LastName Bl;
+        String FirstName Ap;
+        String LastName Bp;
+        Map map=new Hash Map();
+        map.put("First Name A");
+        map.put("Last Name B");
+        map.put("First Name A");
+        map.put("Last Name B");
+        System.out.println(map get "First Name");
+
+    }
+}
